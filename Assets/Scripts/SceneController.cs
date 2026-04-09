47152e44f00eb73eda5e93465ac0288c4dfc7339
chxbim <christianbimo08@gmail.com>
2026-04-09 14:53:13 +0700

    add SceneController
    Scene mgmt scripts, including future button functions

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,26 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadMainGame()
     {
+        SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void LoadSelectMode()
+    {
         SceneManager.LoadScene("SelectMode");
+    }
+
+    public void LoadProfileScene()
+    {
+        SceneManager.LoadScene("ProfileScene");
+    }
+
+    public void LoadSettingScene()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+
+    public void LoadHomeScene()
+    {
+        SceneManager.LoadScene("HomeScene");
     }
 }
