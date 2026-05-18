70eb782c0337cb306c16fde06ace361b56c848a2
Chris. Bimo <christianbimo08@gmail.com>
2026-05-18 17:02:40 +0700

    feat beta login system

---
+++ Assets/Scripts/AuthSys/AuthManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+
+public class AuthManager : MonoBehaviour
+{
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+
+}
